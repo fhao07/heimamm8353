8426e23198f304a4360057f2b752b4d9303d6192
fhao07 <1443933775@qq.com>
2020-05-21 00:02:26 +0800

    4-layout页面布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import layout from '@/views/layout';
-import login from '@/views/login'
+import login from '@/views/login';
+import { getToken } from '@/utils/token';
+import chart from '@/views/layout/chart';
+import enterprise from '@/views/layout/enterprise';
+import question from '@/views/layout/question';
+import subject from '@/views/layout/subject';
+import user from '@/views/layout/user';
 Vue.use(VueRouter);
 
 let router = new VueRouter({
@@ -12,7 +18,29 @@ let router = new VueRouter({
         },
         {
             path: '/layout',
-            component: layout
+            component: layout, children: [
+                {
+                    path: 'chart',
+                    component: chart
+                },
+                {
+                    path: 'enterprise',
+                    component: enterprise
+                },
+                {
+                    path: 'question',
+                    component: question
+                },
+                {
+                    path: 'subject',
+                    component: subject
+                },
+                {
+                    path: 'user',
+                    component: user
+                }
+            ]
+
         },
         {
             path: '/login',
@@ -20,4 +48,23 @@ let router = new VueRouter({
         }
     ]
 });
+//导航守卫
+router.beforeEach((to, from, next) => {
+    // console.log(getToken);
+
+    // console.log(to);
+    // console.log(from);
+    if (to.path == '/login') {
+        next()
+    } else {
+        let token = getToken();
+        if (token) {
+            next()
+        } else {
+            next('/login')
+        }
+    }
+
+
+})
 export default router
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,39 @@
 import Vue from 'vue';
 import axios from 'axios';
+import { getToken, removeToken } from "@/utils/token";
+import router from '@/router'
 // 设置基础路径
 axios.defaults.baseURL = process.env.VUE_APP_BASEURL;
 // 允许携带cookies
 axios.defaults.withCredentials = true;
 
+// Add a request interceptor
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    // console.log('--------------', config);
+    if (getToken()) {
+        config.headers.token = getToken();
 
+    }
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+    // Do something with response data
+    console.log(response);
+    if (response.data.code == 206) {
+        removeToken()
+        //   window.location.reload();
+        router.push('/login')
+        return
+    }
+    return response;
+}, function (error) {
+    // Do something with response error
+    return Promise.reject(error);
+});
 Vue.prototype.$axios = axios;
